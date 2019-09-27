97448442a774b89c0d04b8fe7988a90ceec09c36
RainNameless <namelessrainde@gmail.com>
2019-09-27 16:52:27 +0800

    更新

--- go_test/2.数据类型.go
+++ go_test/2.数据类型.go
@@ -3,22 +3,15 @@ package main
 import "fmt"
 
 /* 	Go 语言数据类型包含基础类型和复合类型两大类。
-	基础数据类型包括：布尔型、整型、浮点型、复数型、字符型、字符串型、错误类型。
-	复合数据类型包括：指针、数组、切片、字典、通道、结构体、接口。
+基础数据类型包括：布尔型、整型、浮点型、复数型、字符型、字符串型、错误类型。
+复合数据类型包括：指针、数组、切片、字典、通道、结构体、接口。
 */
 
-func main(){
+func main() {
 
-	var a bool = true		 /* 布尔型的值只可以是常量 true 或者 false */
-	fmt.Println("bool=",a)
+	//var a bool = true		 /* 布尔型的值只可以是常量 true 或者 false */
+	var b uint8 = 255    /* uint8,无符号8位整形,存储空间8bit，值范围0 ~ 255. */ /*  */
+	var c uint16 = 65535 /* uint16,无符号16位整形,存储空间16bit,值范围0 ~65535 */
 
-	var b uint8 = 255 		/* uint8,无符号8位整形,存储空间8bit，值范围0 ~ 255. */ /*  */
-	var c uint16=65535		/* uint16,无符号16位整形,存储空间16bit,值范围0 ~65535 */
-	var d
-
-
-
-	fmt.Println("uint8=",b,c)
-	fmt.Println()
-	fmt.Println()
+	fmt.Println("uint8=", b, c)
 }
